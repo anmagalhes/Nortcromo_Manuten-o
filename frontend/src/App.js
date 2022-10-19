9c3225bf9be1f65e91679f35892c695542649e37
DanteDerette <sdanted@hotmail.com>
2022-10-19 17:14:56 -0300

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import Button from '@mui/material/Button';
+
+const drawerWidth = 240;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar
+        position='fixed'
+        sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
+      >
+        <Toolbar>
+          <Typography variant='h6' noWrap component='div'>
+            Sistema de Controle Interno
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Drawer
+        variant='permanent'
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            {/* <img src='./north_cromo.png' width={220} height={215} /> */}
+
+            <ListItem disablePadding>
+              <ListItemButton>Ordem de Serviço</ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>Clientes</ListItemButton>
+            </ListItem>
+            <ListItem disablePadding>
+              <ListItemButton>Setores</ListItemButton>
+            </ListItem>
+          </List>
+        </Box>
+      </Drawer>
+      <Box component='main' sx={{ flexGrow: 1, p: 3 }}>
+        <Toolbar />
+      </Box>
+    </Box>
   );
 }
-
 export default App;
